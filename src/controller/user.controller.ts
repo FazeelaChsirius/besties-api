b63972f9a5208aa9efee0e603479dbccb80ef9a8
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2025-08-30 22:55:05 +0500

    Error Handling in Dev and Prod security

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import AuthModel from "../model/auth.model"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { CatchError, TryError } from "../utils/error"
 
 const accessTokenExpiry = '10m'
 
@@ -10,6 +11,7 @@ interface PayloadInterface {
     email: string
     mobile: string
 }
+
 
 const generateToken = (payload: PayloadInterface) => {
     const accessToken = jwt.sign(payload, process.env.AUTH_SECRET!, {expiresIn: accessTokenExpiry})
@@ -21,8 +23,9 @@ export const signup = async (req: Request, res: Response) => {
         await AuthModel.create(req.body)
         res.status(200).json({message: 'Signup success'})
         
-    } catch (err: any) {
-        res.status(500).json({message: err.message})
+    } catch (err: unknown) {
+        if(err instanceof Error)
+            res.status(500).json({message: err.message})
     }
 }
 
@@ -32,12 +35,12 @@ export const login = async (req: Request, res: Response) => {
         const user = await AuthModel.findOne({email})
 
         if(!user)
-            throw new Error('User not found, please signup!')
+            throw TryError("User not found, please signup!", 401)
         
         const isLogin = await bcrypt.compare(password, user.password)
-
+        
         if(!isLogin)
-            throw new Error('Invalid credentials email or password is incorrect')
+            throw TryError("Invalid credentials email or password is incorrect", 404)
 
         const payload = {
             id: user._id,
@@ -55,8 +58,8 @@ export const login = async (req: Request, res: Response) => {
         res.cookie('accessToken', accessToken, options)
         res.json({message: 'Login Successfully'})
         
-    } catch (err: any) {
-        res.status(500).json({message: err.message})
+    } catch (err: unknown) {
+        CatchError(err, res)
     }
 }
 
