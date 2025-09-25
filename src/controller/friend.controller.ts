24c6ae8db9f605b69b5ae85c963d087f22b0980d
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2025-09-25 21:20:07 +0500

    Working On Friend And Friend Suggestion Engine

--- src/controller/friend.controller.ts
+++ src/controller/friend.controller.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from "express"
-import { CatchError } from "../utils/error"
+import { Response } from "express"
+import { CatchError, TryError } from "../utils/error"
 import FriendModel from "../model/friend.model"
 import { SessionInterface } from "../middleware/auth.middleware"
 import AuthModel from "../model/auth.model"
+import mongoose from "mongoose"
 
 export const addFriend = async (req: SessionInterface, res: Response) => {
     try {
         req.body.user = req.session?.id 
         const friend = await FriendModel.create(req.body)
         res.json(friend)
-        
         
     } catch (err) {
         CatchError(err, res, "Failed to send friend request")
@@ -29,14 +29,29 @@ export const fetchFriends = async (req: SessionInterface, res: Response) => {
 
 export const suggestedFriends = async (req: SessionInterface, res: Response) => {
     try {
+        if(!req.session)
+            throw TryError("Fail to suggest friend", 401)
+            
         const friends = await AuthModel.aggregate([
+            {
+                $match: {
+                    _id: {$ne: new mongoose.Types.ObjectId(req.session.id)}
+                }
+            },
             {$sample: {size: 5}},
-            {$project: {fullname: 1, image: 1}}
+            {$project: {fullname: 1, image: 1, createdAt: 1}}
         ])
-        res.json(friends)
+
+        const modified = await Promise.all(
+            friends.map(async (item) => {
+                const count = await FriendModel.countDocuments({friend: item._id})
+                return count === 0 ? item : null
+            })
+        )
+        const filtered = modified.filter((item) => item !== null)
+        res.json(filtered)
        
     } catch (err) { 
         CatchError(err, res, "Failed to fetch friends")
     }
-
 }
