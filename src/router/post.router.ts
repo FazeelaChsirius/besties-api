d6e6909873dcfcc4abe858683d00559a6b3c031e
fazeelamushtaq <fazeela.mushtaq.sirius@gmail.com>
2025-11-11 18:18:14 +0500

    Backend Design For Social Media Posts

---
+++ src/router/post.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express"
+import { createPost, fetchPosts } from "../controller/post.controller"
+
+const PostRouter = Router()
+PostRouter.post('/', createPost)
+PostRouter.get('/', fetchPosts)
+
+export default PostRouter
